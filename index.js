e1a9d5052850fb238f129b0e50428c03bd5e733b
DESKTOP-3ERO3H9\neetstyle <neetstyle.net>
2024-08-01 17:51:49 +0900

    a

--- index.js
+++ index.js
@@ -48,19 +48,19 @@ if (reversed == null) { reversed = false; }
 
 	// レイヤー_1
 	this.instance = new lib.images();
-	this.instance.setTransform(0,0,2.2222,2.2222);
+	this.instance.setTransform(0,0,2.2222,4.4444);
 
 	this.timeline.addTween(cjs.Tween.get(this.instance).wait(1));
 
 	this._renderFirstFrame();
 
 }).prototype = p = new lib.AnMovieClip();
-p.nominalBounds = new cjs.Rectangle(250,250,250,250);
+p.nominalBounds = new cjs.Rectangle(250,500,250,500);
 // library properties:
 lib.properties = {
 	id: '2B00AFE24692F54B994DDA463BA583A4',
 	width: 500,
-	height: 500,
+	height: 1000,
 	fps: 30,
 	color: "#FFFFFF",
 	opacity: 1.00,
