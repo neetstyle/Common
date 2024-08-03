3a5543150b16f9f2d0e7d2754c344a14436319f2
DESKTOP-3ERO3H9\neetstyle <neetstyle.net>
2024-08-03 15:47:14 +0900

    a

--- index.js
+++ index.js
@@ -704,35 +704,6 @@ if (reversed == null) { reversed = false; }
 		
 		
 		
-		function startScroll1(event) {
-		stage.getChildByName("aaaaaa").log1.text = new Date().getTime();	
-		}
-		
-		function onScroll1(event) {
-		stage.getChildByName("aaaaaa").log2.text = new Date().getTime();	
-		}
-		
-		function endScroll1(event) {
-		stage.getChildByName("aaaaaa").log3.text = new Date().getTime();	
-		}
-		
-		function touchEventPolyfill(element) {
-		    element.addEventListener("mousedown", function(event) {
-		        startScroll1(event);
-		        window.addEventListener("mousemove", onScroll1);
-		        window.addEventListener("mouseup", function endMouseScroll1(event) {
-		            endScroll1(event);
-		            window.removeEventListener("mousemove", onScroll1);
-		            window.removeEventListener("mouseup", endMouseScroll1);
-		        });
-		    });
-		}
-		
-		touchEventPolyfill(content);
-		
-		
-		
-		
 		
 		
 		
@@ -963,7 +934,7 @@ if (reversed == null) { reversed = false; }
 	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1));
 
 	// レイヤー_2
-	this.text = new cjs.Text("9", "50px 'MS Gothic'", "#FF0000");
+	this.text = new cjs.Text("10", "50px 'MS Gothic'", "#FF0000");
 	this.text.lineHeight = 52;
 	this.text.lineWidth = 90;
 	this.text.parent = this;
