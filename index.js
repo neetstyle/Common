e0bb66a6aa4d30f4dea02ded3fcb33e7e4dfe586
DESKTOP-3ERO3H9\neetstyle <neetstyle.net>
2024-08-03 15:34:20 +0900

    a

--- index.js
+++ index.js
@@ -686,12 +686,12 @@ if (reversed == null) { reversed = false; }
 		var lastMoveTime;
 		
 		// マウスイベントのリスナーを追加
-		//content.addEventListener("mousedown", startScroll);
-		//stage.addEventListener("stagemousemove", doScroll);
-		//stage.addEventListener("stagemouseup", endScroll);
+		content.addEventListener("mousedown", startScroll);
+		stage.addEventListener("stagemousemove", doScroll);
+		stage.addEventListener("stagemouseup", endScroll);
 		
 		// タッチイベントのリスナーを追加
-		content.addEventListener("touchstart", startScroll);
+		//content.addEventListener("touchstart", startScroll);
 		//content.addEventListener("touchstart", startScroll, { capture: true });
 		//stage.addEventListener("touchmove", doScroll);
 		//stage.addEventListener("touchend", endScroll);
@@ -926,7 +926,7 @@ if (reversed == null) { reversed = false; }
 	this.timeline.addTween(cjs.Tween.get(this).call(this.frame_0).wait(1));
 
 	// レイヤー_2
-	this.text = new cjs.Text("7", "50px 'MS Gothic'", "#FF0000");
+	this.text = new cjs.Text("8", "50px 'MS Gothic'", "#FF0000");
 	this.text.lineHeight = 52;
 	this.text.lineWidth = 90;
 	this.text.parent = this;
